4cbec4af5ca3cc157fffe77392458e3ee9aeeb21
cheese-2d <162503099+cheese-2d@users.noreply.github.com>
2024-04-24 11:36:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-(function() {
-    'use strict';
+// (function() {
+//     'use strict';
 
-    // Replace the entire HTML content of the page
-    document.documentElement.innerHTML = `
-                <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Hacked</title>
-            <style>
-                body {
-                    background-color: black;
-                    color: #fff;
-                    width: 100vw;
-                    height: 100vh;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                }
-            </style>
-        </head>
-        <body>
-            <h1>YOU HAVE BEEN HACKED</h1>
-            <script>alert("manny your a fag")</script>
-        </body>
-        </html>
-    `;
-})();
+//     // Replace the entire HTML content of the page
+//     document.documentElement.innerHTML = `
+//                 <!DOCTYPE html>
+//         <html lang="en">
+//         <head>
+//             <meta charset="UTF-8">
+//             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//             <title>Hacked</title>
+//             <style>
+//                 body {
+//                     background-color: black;
+//                     color: #fff;
+//                     width: 100vw;
+//                     height: 100vh;
+//                     display: flex;
+//                     justify-content: center;
+//                     align-items: center;
+//                 }
+//             </style>
+//         </head>
+//         <body>
+//             <h1>YOU HAVE BEEN HACKED</h1>
+//             <script>alert("manny your a fag")</script>
+//         </body>
+//         </html>
+//     `;
+// })();
